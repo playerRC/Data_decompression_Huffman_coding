e4384d08d89ecd310043de88c9970d44ba138476
unknown <rachid.estrada@gmail.com>
2022-05-11 23:37:19 +0200

    dictionnaire caractere-frequence

--- project/frequency.java
+++ project/frequency.java
@@ -5,26 +5,32 @@ import java.util.HashMap;
 
 // help https://gayerie.dev/epsi-b3-java/langage_java/attributs_et_methodes.html
 
-public class Frequence{
+public class Frequency{
     private String nom;
-    private File doc = new File(nom);
 
-    public Frequence(nom, doc){
+    public Frequency(String nom){
         this.nom = nom;
-        this.doc = doc;
     }
 
-    public ArrayList<Integer> listFreq(doc, nom) {
-        BufferedReader obj = new BufferedReader(new FileReader(doc));
-        HashMap<String, String> frequ = new HashMap<String, String>();
-        
-        // String&#91;&#93; line
-        String line;
-        while ((line = obj.readLine()) != null){
-            if (line.length() == 3){
-                frequ.put(line.charAt(0), line.charAt(3));
-            }
+    public HashMap<String, String> listFreq() {
+        File doc = new File(this.nom);
+        HashMap<String, String> frequ = new HashMap<String, String>(); 
+        try{
+          // Le fichier d'entrée
+          BufferedReader obj = new BufferedReader(new FileReader(doc)); 
+          
+          //renvoie true s'il y a une autre ligne à lire
+          String line;
+          while ((line = obj.readLine()) != null){
+              if (line.length() == 3){
+                  frequ.put(String.valueOf(line.charAt(0)), String.valueOf(line.charAt(2)));
+              }
+          }   
         }
+        catch(IOException e){
+          e.printStackTrace();
+        }
+        return frequ;
     }
 
 }
--- project/main.java
+++ project/main.java
@@ -4,22 +4,7 @@ public class Main
 {
   public static void main(String args[])
   {
-    try
-    {
-      // Le fichier d'entrée
-      FileInputStream file = new FileInputStream("exemple_freq.txt");   
-      Scanner scanner = new Scanner(file);  
-      
-      //renvoie true s'il y a une autre ligne à lire
-      while(scanner.hasNextLine())
-      {
-        System.out.println(scanner.nextLine());
-      }
-      scanner.close();    
-    }
-    catch(IOException e)
-    {
-      e.printStackTrace();
-    }
+    Frequency f = new Frequency("exemple_freq.txt");
+    System.out.println(f.listFreq());
   }
 }
